02ca3dac20e82429592478360b0eaa34983b034d
Hannu Parviainen <hpparvi@gmail.com>
2016-05-30 12:17:41 +0100

    Modified setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from numpy.distutils.misc_util import Configuration
 import distutils.sysconfig as ds
 
 setup(name='KeplerJC',
-      version='0.1',
-      description='',
+      version='0.5',
+      description='Jump (discontinuity) detection, classification, and correction for Kepler light curves',
       author='Hannu Parviainen',
       author_email='hpparvi@gmail.com',
-      url='',
+      url='https://github.com/hpparvi/KeplerJC',
       package_dir={'keplerjc':'src'},
       packages=['keplerjc']
      )
